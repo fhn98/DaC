02b62678767b8d81ec2256125d07d6d6cc980a6d
Fahimeh Hosseini <fahim.hosseini.77@gmail.com>
2024-06-19 14:44:28 +0330

    updated main

--- main.py
+++ main.py
@@ -5,25 +5,10 @@ import random
 import argparse
 import copy
 from models import ResNet50
-from data import *
 from test import test
-from train import train_masked_low_loss
-from utils import weight_init, compute_loss_quantiles
-
-
-def get_loaders(dataset, path, batch_size=32, mask_path=None, get_mask=False, get_names=False):
-    if dataset == 'WaterBirds':
-        return get_waterbird_loaders(path=path, batch_size=batch_size, mask_path=mask_path, get_mask=get_mask,
-                                     get_names=get_names)
-    elif dataset == 'CelebA':
-        return get_celeba_loaders(path=path, batch_size=batch_size, get_mask=get_mask, mask_path=mask_path,
-                                  get_names=get_names)
-    elif dataset == 'MetaShift':
-        return get_metashift_loaders(batch_size=batch_size, path=path, mask_path=mask_path, get_mask=get_mask,
-                                     get_names=get_names)
-    elif dataset == 'Domino':
-        return get_domino_loaders(path=path, batch_size=batch_size, mask_path=mask_path, get_mask=get_mask,
-                                  get_names=get_names)
+from train import train_masked_low_loss, train_erm
+from utils import *
+from data.utils import get_loaders
 
 
 def main(args):
@@ -35,40 +20,48 @@ def main(args):
 
     print('Dataloaders prepared')
     model = ResNet50().to(device)
-    model.load_state_dict(torch.load(args.model_path))
 
-    base_model = ResNet50().to(device)
-    base_model.load_state_dict(torch.load(args.model_path))
+    if not args.experiment == 'ERM':
+        model.load_state_dict(torch.load(args.model_path))
 
-    model.eval()
-    test(testloader, model, args)
+        model.eval()
+        print('acc of the ERM model on the test set:')
+        test(testloader, model, args)
 
-    t = compute_loss_quantiles(trainloader, base_model, args.quantile)
-    print('loss threshold', t)
+        base_model = ResNet50().to(device)
+        base_model.load_state_dict(torch.load(args.model_path))
 
-    for n, p in model.named_parameters():
-        p.requires_grad = False
+        t = compute_loss_quantiles(trainloader, base_model, args.quantile)
+        print('loss threshold', t)
 
-    weight_init(model.model.fc)
+        for n, p in model.named_parameters():
+            p.requires_grad = False
 
-    for p in model.model.fc.parameters():
-        p.requires_grad = True
+        weight_init(model.model.fc)
 
-    trainable_parameters = model.model.fc.parameters()
+        for p in model.model.fc.parameters():
+            p.requires_grad = True
 
-    model_optimizer = torch.optim.Adam(trainable_parameters, lr=args.lr)
-    scheduler = torch.optim.lr_scheduler.StepLR(model_optimizer, step_size=args.step_size, gamma=args.gamma)
+    if args.experiment == 'ERM':
+        trainable_parameters = model.parameters()
+
+    else:
+        trainable_parameters = model.model.fc.parameters()
+
+    model_optimizer = get_optimizer(trainable_parameters, args.lr, args.optimizer, args.weight_decay)
+    scheduler = get_scheduler(model_optimizer, args)
 
     ######################
     # learning
     ######################
     global_step = 0
 
-    for p in base_model.parameters():
-        p.requires_grad = True
+    if not args.experiment == 'ERM':
+        for p in base_model.parameters():
+            p.requires_grad = True
 
     best_worst = 0
-    best_cnn = None
+    best_model = None
     best_avg = 0
 
     for epoch in range(args.num_epochs):
@@ -76,35 +69,40 @@ def main(args):
         print("epoch:", epoch)
         print("=========================")
         model.train()
-        base_model.eval()
 
-        global_step = train_masked_low_loss(trainloader, model, base_model, model_optimizer,
-                                            scheduler, global_step, t=t, args=args)
+        if not args.experiment == 'ERM':
+            base_model.eval()
+
+            global_step = train_masked_low_loss(trainloader, model, base_model, model_optimizer,
+                                                scheduler, global_step, t=t, args=args)
+
+        else:
+            global_step = train_erm(trainloader, model, model_optimizer, scheduler, global_step)
 
         # dev
         print('acc on val ....')
         avg_acc, envs_acc = test(valloader, model, args)
         if min(envs_acc) > best_worst:
             best_worst = min(envs_acc)
-            best_cnn = copy.deepcopy(model)
+            best_model = copy.deepcopy(model)
             best_avg = avg_acc
 
         elif (min(envs_acc) == best_worst and avg_acc > best_avg):
             best_worst = min(envs_acc)
-            best_cnn = copy.deepcopy(model)
+            best_model = copy.deepcopy(model)
             best_avg = avg_acc
 
         print('acc on test ....')
         test(testloader, model, args)
 
-    model.load_state_dict(best_cnn.state_dict())
+    model.load_state_dict(best_model.state_dict())
     model.eval()
 
     print('best model acc on val:')
     test(valloader, model, args)
 
     print('best model acc on test:')
-    avg_acc, envs_acc = test(testloader, model, args)
+    test(testloader, model, args)
 
     torch.save(model.state_dict(), args.save_path + f'alpha{args.alpha}_lt{args.quantile}_bs{args.batch_size}.model')
 
@@ -112,25 +110,29 @@ def main(args):
 if __name__ == "__main__":
     seed = 10
     parser = argparse.ArgumentParser()
-    default_data_path = '/home/f_hosseini/data/metashifts/metashifts/MetaDatasetCatDog'
-    default_mask_path = '/home/f_hosseini/data/metashift_masks/'
-    default_model_path = '/home/f_hosseini/dfr-ckpts/metashift/metashift_erm_run1.pt'
-    default_save_path = '/home/f_hosseini/results/dominoes/'
+    default_data_path = '/HDD/Datasets/Waterbirds_dataset/waterbird_complete95_forest2water2/'
+    default_mask_path = '/home/f_hosseini/data/waterbirds_masks'
+    default_model_path = '/home/f_hosseini/dfr-ckpts/waterbirds/erm_seed1/final_checkpoint.pt'
+    default_save_path = '/home/f_hosseini/results/wb/'
     parser.add_argument("--data_path", type=str, default=default_data_path, help="data path")
-    parser.add_argument("--mask_path", type=str, default=default_mask_path, help="data path")
+    parser.add_argument("--mask_path", type=str, default=default_mask_path, help="mask path")
     parser.add_argument("--model_path", type=str, default=default_model_path, help="pretrained model path")
     parser.add_argument("--save_path", type=str, default=default_save_path, help="path to save checkpoints")
-    parser.add_argument("--dataset", type=str, default='MetaShift')
+    parser.add_argument("--experiment", type=str, default='DaC', help="The experiment to run", choices=['ERM', 'DaC'])
+    parser.add_argument("--dataset", type=str, default='WaterBirds')
     parser.add_argument("--batch_size", type=int, default=32, help="batch_size")
     parser.add_argument("--num_classes", type=int, default=2)
     parser.add_argument("--num_envs", type=int, default=4)
-    parser.add_argument("--num_test_envs", type=int, default=2)
+    parser.add_argument("--num_test_envs", type=int, default=4)
     parser.add_argument("--num_epochs", type=int, default=15)
     parser.add_argument("--lr", type=float, default=0.005)
+    parser.add_argument("--weight_decay", type=float, default=0)
+    parser.add_argument("--optimizer", type=str, default='Adam', choices=['Adam', 'SGD'])
+    parser.add_argument("--scheduler", type=str, default='none', choices=['none', 'StepLr'])
     parser.add_argument("--step_size", type=float, default=5)
     parser.add_argument("--gamma", type=float, default=0.5)
-    parser.add_argument("--alpha", type=float, default=6)
-    parser.add_argument("--quantile", type=float, default=0.8)
+    parser.add_argument("--alpha", type=float, default=10)
+    parser.add_argument("--quantile", type=float, default=0.6)
     parser.add_argument("--invert_mask", type=bool, default=False)
     parser.add_argument("--seed", type=int, default=10)
 
